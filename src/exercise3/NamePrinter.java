d7b3cc1d25a5ce2f00c8b0d49aa4b6f94c796b76
HNeychev <>
2021-10-27 15:41:05 +0000

    FikiFikiFiki

--- src/exercise3/NamePrinter.java
+++ src/exercise3/NamePrinter.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class NamePrinter {
 
     public static void main(String[] args) {
-        System.out.println("Enter a word: ");
         Scanner input = new Scanner(System.in);
         String letter = input.nextLine();
       

2492c44b55e908d6c3f17ca6d21506a48ac0e639
HNeychev <>
2021-11-02 08:18:59 +0000

    opit 201

--- src/exercise3/AngleCalculation.java
+++ src/exercise3/AngleCalculation.java
@@ -1,29 +1,28 @@
 package exercise3;
-import java.lang.Math;
+
 import java.util.Scanner;
 
 public class AngleCalculation {
-  public static void main(String[] args) {
 
-Scanner input = new Scanner(System.in); 
-    char type ;
-    double value ;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-    do{
-      value = input.nextDouble();
-      type = input.next().charAt(0);
+        double value;
+        char type;
+        do {
+          value = input.nextDouble();
+          type = input.next().charAt(0);
 
-      if (type == 'r'){
+          if (type == 'r') {
+            double convertedValue = value / Math.PI * 180;
+            System.out.println("Angle: " + convertedValue + "d");
 
-        double convertedValue1 = value / Math.PI * 180;
-        System.out.println("Angel: " + convertedValue1 + 'd');
+          }else if (type == 'd') {
+            double convertedValue = value * Math.PI / 180;
+            System.out.println("Angle: " + convertedValue + "r");
+          }
 
-      }else if (type == 'r'){
-        double convertedValue2 = value * Math.PI / 180;
-        System.out.println("Angle:" + convertedValue2 + 'r');
-      }
-    }while(type == 'r' || type == 'd' );
-    
+        } while (type == 'r' || type == 'd' );
     }
 
 }
